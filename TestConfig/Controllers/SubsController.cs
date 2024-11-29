8d312a995f36e5d2d8550fb2b23fc7e9ab79c386
JHooper <jhooper@broughtonsoftware.com>
2024-11-29 15:44:10 +0000

    test: assert data in controller
    yes, I know this is bad practice, but I want to actually test the data being injected by the IConfiguration builder

--- TestConfig/Controllers/SubsController.cs
+++ TestConfig/Controllers/SubsController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace TestConfig.Controllers;
 
@@ -10,6 +11,20 @@ public class SubsController(IOptions<SubscriptionTiers> opts) : ControllerBase
     [HttpGet]
     public SubscriptionTiers Get()
     {
+        TestInjectedData();
         return opts.Value;
+    }
+
+    private void TestInjectedData()
+    {
+        var free = opts.Value.First(x => x.Tier == "Free");
+
+        // This is failing when it should only have 1 empty string
+        Assert.AreEqual(1, free.ActiveModules.Count());
+        Assert.AreEqual(0, free.ActiveModules.Where(x => !string.IsNullOrWhiteSpace(x)).Count());
+
+        var other = opts.Value.First(x => x.Tier == "Growth");
+        // Again this is failing when there are only 2 values configured
+        Assert.AreEqual(2, other.ActiveModules.Count());
     }
 }
